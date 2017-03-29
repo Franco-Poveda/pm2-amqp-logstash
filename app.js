1929ad911af5e742a45ba6ef68be9468b86972f5
J. Federico Hernandez <fede.hernandez@gmail.com>
2017-03-29 16:25:53 -0300

    Added more amqp and log config params.

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
-
-var bunyan = require('bunyan');
-var pm2 = require('pm2');
-var pmx = require('pmx');
+const bunyan = require('bunyan');
+const pm2 = require('pm2');
+const pmx = require('pmx');
 
 pmx.initModule({
-
   widget: {
-
     logo: 'http://semicomplete.com/presentations/logstash-monitorama-2013/images/elasticsearch.png',
-
     theme: ['#141A1F', '#222222', '#3ff', '#3ff'],
-
     el: {
       probes: false,
       actions: false
     },
-
     block: {
       actions: false,
       issues: false,
       meta: false,
-
     }
-
+  }
+}, (err, conf) => {
+  if (err) {
+      console.log('error', err);
+      return;
   }
 
-}, function (err, conf) {
-
-  var amqp_stream = require('bunyan-logstash-amqp').createStream({
-    host: conf.host,
+  const amqpStream = require('bunyan-logstash-amqp').createStream({
+    host: conf.amqpHost,
+    vhost: conf.amqpVhost || 'sandbox',
     exchange: {
-      name: conf.exchange,
-      routingKey: 'test'
-    }
+      name: conf.amqpExchange,
+      routingKey: conf.amqpRoutingKey
+    },
+    login: conf.amqpUser,
+    password: conf.amqpPasswd
   })
-    .on('connect', function () {
-      console.log("Connected to amqp [module]");
-    })
-    .on('close', function () { console.log("Closed connection to amqp"); })
+    .on('connect', () => console.log("Connected to amqp [module]"))
+    .on('close', () => console.log("Closed connection to amqp"))
     .on('error', console.log);
 
-  var log = bunyan.createLogger({
-    name: 'log-stream',
+  const log = bunyan.createLogger({
+    name: conf.logName,
     streams: [{
-      level: 'debug',
+      level: conf.logLevel,
       type: "raw",
-      stream: amqp_stream
+      stream: amqpStream
     }],
-    level: 'debug'
+    level: conf.logLevel
   });
 
-  pm2.connect(function () {
+  pm2.connect(() => {
     console.log('info', 'PM2: forwarding to amqp');
-    pm2.launchBus(function (err, bus) {
+
+    pm2.launchBus((err, bus) => {
       bus.on('log:PM2', function (packet) {
         log.debug(packet.data);
-      })
+      });
       bus.on('log:out', function (packet) {
         log.debug(packet.data);
       });
@@ -63,5 +60,6 @@ pmx.initModule({
         log.error(packet.data);
       });
     });
-  })
+  });
+
 });
